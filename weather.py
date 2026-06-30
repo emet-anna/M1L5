800bd1aaa198c5bceaf8ae1b454f7a91c21373e3
Anna_Emet <karanjann.codingal@gmmail.com>
2026-06-30 17:25:27 +0300

    cold or hot weather checker

---
+++ weather.py
@@ -0,0 +1,9 @@
+#check whether it is hot or cold
+weather=input("Is it sunny or cloudy?: ")
+if weather=='sunny':
+    print('It is very hot, let\'s get an icecream!')
+else:
+    print('Cold today, keep warm!')
+    
+    
+    
