1853e136eb95a726b43085566d34c78789a36e52
Anna_Emet <karanjann.codingal@gmmail.com>
2026-06-30 17:59:23 +0300

    Trip paid or not

---
+++ trip.py
@@ -0,0 +1,9 @@
+#checking if a child has paid and can then go for a trip
+has_paid=input('Has the child paid trip money?: ')
+
+if has_paid=='Yes.':
+    print('Yes the child has paid. They can go for the trip')
+else:
+    print('No.The child will be staying at home on that day.')
+    
+
