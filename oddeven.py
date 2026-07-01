23520284e1794bd06d4a06a9047d9a3627d7f6ec
Anna_Emet <karanjann.codingal@gmmail.com>
2026-07-01 14:57:50 +0300

    odd and even numbers

---
+++ oddeven.py
@@ -0,0 +1,10 @@
+'''Odd-even
+Short description:
+Write a program to check whether the given number is even or odd?'''
+
+num=int(input('enter your number: '))
+if num % 2 == 0:
+    print(num, "is an even number")
+else:
+    print(num, "is an odd number")
+    
